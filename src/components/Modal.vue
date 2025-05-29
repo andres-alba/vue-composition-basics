3b1b566722779c59e75e872797386a499f06bc96
Andres Alba <andresalba34@gmail.com>
2025-05-29 12:34:03 -0400

    enable lazy loading for components

---
+++ src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+  <teleport to=".modals-container">
+    <div class="modal">
+      <h1>This is a modal</h1>
+      <p>Lorem ipsijm dolor</p>
+      <button>Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<style>
+.modal {
+  background: red;
+  padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -1,33 +1,15 @@
 <template>
   <div class="modals">
     <h1>Modals</h1>
+    <Modal v-if="showModal"></Modal>
     <button @click="showModal = true">Show modal</button>
-    <teleport to=".modals-container">
-      <div v-if="showModal" class="modal">
-        <h1>This is a modal</h1>
-        <p>Lorem ipsijm dolor</p>
-        <button @click="showModal = false">Hide modal</button>
-      </div>
-    </teleport>
   </div>
 </template>
 <script setup>
 // imports
 import { ref } from 'vue'
+import Modal from '@/components/Modal.vue'
 
 //modals
 const showModal = ref(false)
 </script>
-
-<style>
-.modal {
-  background: red;
-  padding: 10px;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-}
-</style>
